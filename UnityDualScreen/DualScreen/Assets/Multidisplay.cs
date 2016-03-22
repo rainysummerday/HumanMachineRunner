3e228dfa3c9ad1f99292e7ba2bfc139e9a07a954
Sarah <sarah.busert@hs-furtwangen.de>
2016-03-22 22:15:17 +0100

    Added dual screen test project

---
+++ UnityDualScreen/DualScreen/Assets/Multidisplay.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Multidisplay : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        Debug.Log(Display.displays.Length);
+        if (Display.displays.Length.Equals(2))
+            Display.displays[1].Activate();
+    }
+}
+	
+
